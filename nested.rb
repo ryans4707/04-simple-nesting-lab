5ea728a62afd2466434311bac0ffb76acf5a190a
ryans4707 <ryans4707@hstat.org>
2016-11-16 05:22:58 +0000

    Finished hw

--- nested.rb
+++ nested.rb
@@ -17,7 +17,9 @@ def hopper
  		}
     
   # CODE HERE
-    
+ programmer_hash[:grace_hopper]
+ 
+ 
 end
 
 def alan_kay_is_known_for
@@ -40,7 +42,7 @@ def alan_kay_is_known_for
      
   # What combination of keys would you use to return the value of the :known_for key of :alan_kay?
   # CODE HERE
-     
+     programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -61,7 +63,7 @@ def dennis_ritchies_language
       }
     }
   # CODE HERE
-  
+  programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
@@ -88,7 +90,8 @@ def adding_matz
   # VALUE
   # {:known_for => "Ruby", :languages => ["LISP, C"]}
   # CODE HERE
-  
+  programmer_hash[:yukihiro_matsumoto]={:known_for => "Ruby", :languages => ["LISP, C"]}
+  programmer_hash
 end
 
 def changing_alan
@@ -111,7 +114,8 @@ def changing_alan
   
   # change what Alan Kay is :known_for to "GUI"
   # CODE HERE
-  
+   programmer_hash[:alan_kay][:known_for] ="GUI"
+  programmer_hash
 end
 
 def adding_to_dennis
@@ -134,5 +138,6 @@ def adding_to_dennis
   
   # adds 'Assembly' to Dennis Ritchie's languages
   # CODE HERE
-  
+  programmer_hash[:dennis_ritchie][:languages] = ["C", "Assembly"]
+  programmer_hash
 end
